1e99a95bbfb0b3058c8bd4ff23c62bbcbcc256e2
thiennguyenhieu <thien.nguyenhieu@gmail.com>
2020-07-21 22:34:12 +0700

    Initialize project

---
+++ lib/fastkara_server.dart
@@ -0,0 +1,11 @@
+/// fastkara_server
+///
+/// A Aqueduct web server.
+library fastkara_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
